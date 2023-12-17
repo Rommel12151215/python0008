fc22f0e9b996031491fd7149d47778e4dbf789c1
Rommel <karlsrommel255@gmail.com>
2023-12-17 15:25:10 -0600

    Python0008

---
+++ main.py
@@ -0,0 +1,25 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark!")
+elif username == "suzanne" and password == "Su74nne":
+ print("Hey there Suzanne!")
+elif username == "jo" and password == "pepino":
+  print("Hey there Jo!")
+else:
+ print("Go away!")
+
+
+season = input("what is your favorite season?")
+if season == "spring":
+ print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+ print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+ print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+ print("Stay warm by the fire and watch the snow fall.")
+else: 
+ print("I don't know that season. Please try again.")
